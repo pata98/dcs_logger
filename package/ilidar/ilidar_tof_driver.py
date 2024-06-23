348105fc0087027071df30e677b4db566be3ef3f
pata98 <yoopata@postech.ac.kr>
2024-06-23 21:17:22 +0900

    modify packet structure

--- package/ilidar/ilidar_tof_driver.py
+++ package/ilidar/ilidar_tof_driver.py
@@ -7,16 +7,14 @@
 #              Sensor @ 192.168.5.x
 #              Host   @ 192.168.5.2
 #
-# EXP mode data packet structure [1302 B] 
+#   EXP mode data packet structure [1298 B] 
 # - STX	                                                [ 2 B] 	(0)
-# - Packet ID (0x0001)									[ 2 B]	(1)
-# - Packet length (1302)								[ 2 B]	(2)
-# - timestamp_us (64 bit)								[ 8 B]	(3~6)
-# - empty (6 bit)  | modfreq (10 bit)					[ 2 B]	(7)
-# - DCSnum (2 bit) | empty (4 bit) | shutter (10 bit)	[ 2 B]	(8)
-# - empty (2 bit)  | frame (6 bit) | row (8 bit)		[ 2 B]	(9)
-# - DATA												[640 x 2 = 1280 B]	(10~649)
-# - ETX													[ 2 B]	(650)
+# - DCSnum (2 bit) | frame (6 bit) | row (8 bit)		[ 2 B]	(1)
+# - timestamp_us (64 bit)								[ 8 B]	(2~5)
+# - empty (6 bit)  | modfreq (10 bit)					[ 2 B]	(6)
+# - empty (6 bit)  | shutter (10 bit)	                [ 2 B]	(7)
+# - DATA												[640 x 2 = 1280 B]	(8~647)
+# - ETX													[ 2 B]	(648)
 #
 ##################################################################
 # iLidar-ToF data retrieval via UDP
@@ -35,7 +33,7 @@ class iLidarDriver():
         # Image information
         self.img_width   = 320
         self.img_height  = 160
-        self.packet_size = 1302
+        self.packet_size = 1298
         self.row_unpack_format = '<' + 'H' * self.img_width # '<': little endian, 'H': unsigned short
         
         # UDP connection
@@ -117,86 +115,38 @@ class iLidarDriver():
             # If received packet is image packet,
             if len(data) == self.packet_size:
                 ## Information packet
-                stx, packet_id, packet_length, timestamp_us, modfreq_shutter, frame_row, dcsnum_empty, data_payload, etx = \
-                    struct.unpack('<2s2sH8s2s2s2s1280s2s', data)
-                packet_id = struct.unpack('<H', packet_id)[0]
-                timestamp_us = struct.unpack('<Q', timestamp_us)[0]
-                modfreq = struct.unpack('<H', modfreq_shutter)[0] & self.MOD_FREQ
-                dcs_num = (struct.unpack('<H', dcsnum_empty)[0] & self.DCS_NUM) >> 14
-                shutter = struct.unpack('<H', modfreq_shutter)[0] & self.SHUTTER
-                frame = (struct.unpack('<H', frame_row)[0] & self.FRAME) >> 8
-                cur_row = struct.unpack('<B', frame_row[1:2])[0]
+                stx = data[0:2]
+                packet_id = struct.unpack('<H', data[2:4])[0]
+                timestamp_us = struct.unpack('<Q', data[4:12])[0]
+                modfreq_shutter = struct.unpack('<H', data[12:14])[0]
+                frame_row = struct.unpack('<H', data[14:16])[0]
+                data_payload = data[16:1296]
+                etx = data[1296:1298]
+                
+                modfreq = modfreq_shutter & self.MOD_FREQ
+                dcs_num = (packet_id & self.DCS_NUM) >> 14
+                shutter = modfreq_shutter & self.SHUTTER
+                frame = (packet_id & self.FRAME) >> 8
+                cur_row = frame_row & self.ROW
 
-                print("dcs_num: {dcs_num}, frame: {frame}, cur_row: {cur_row}, timestamp: {timestamp_us}".\
-                      format(dcs_num=dcs_num, frame=frame, cur_row=cur_row, timestamp_us=timestamp_us))
-                break
+                print(f"dcs_num: {dcs_num}, frame: {frame}, cur_row: {cur_row}, timestamp: {timestamp_us}")
 
+                # Unpack the data payload
+                for y in range(2):
+                    start_index = y * self.img_width * 2
+                    end_index = start_index + self.img_width * 2
+                    pixels_row = struct.unpack(self.row_unpack_format, data_payload[start_index:end_index])
+                    dcs[dcs_num][cur_row + y] = np.array(pixels_row, dtype=np.uint16)
 
-                # EXP mode data packet structure [1302 B] 
-                # - STX	                                                [ 2 B] 	(0)
-                # - Packet ID (0x0001)									[ 2 B]	(1)
-                # - Packet length (1302)								[ 2 B]	(2)
-                # - timestamp_us (64 bit)								[ 8 B]	(3~6)
-                # - empty (6 bit)  | modfreq (10 bit)					[ 2 B]	(7)
-                # - DCSnum (2 bit) | empty (4 bit) | shutter (10 bit)	[ 2 B]	(8)
-                # - empty (2 bit)  | frame (6 bit) | row (8 bit)		[ 2 B]	(9)
-                # - DATA												[640 x 2 = 1280 B]	(10~649)
-                # - ETX													[ 2 B]	(650)
-
-                ## DCS0
-
-
-                ## DCS1
-
-
-                ## DCS2
-
-
-                ## DCS3
-
-
-                """
-                # Depth
-                if cur_row < self.img_height:
-                    for y in range(0, 2):
-                        pixels_row = struct.unpack(self.row_unpack_format, data[(2 + y*2*self.img_width) : (2 + y*2*self.img_width + self.img_width*2)])
-                        pixels_row = np.matrix(pixels_row)
-                        imgDepth[cur_row + y, :] = pixels_row
-                        
-                # Intensity
-                elif cur_row < self.img_height*2:
-                    for y in range(0, 2):
-                        pixels_row = struct.unpack(self.row_unpack_format, data[2 + y*2*self.img_width : 2 + self.img_width*2 + y*2*self.img_width])
-                        pixels_row_masked_1 = np.bitwise_and(pixels_row, 0x1FFF)
-                        pixels_row_masked_2 = np.right_shift(np.bitwise_and(pixels_row, 0xE000), 13)
-                        pixels_row = np.add(pixels_row_masked_1, np.multiply(lv[pixels_row_masked_2], 0x1000))
-                        pixels_row = np.matrix(pixels_row)
-                        imgInten[cur_row-self.img_height + y, :] = pixels_row
+                return dcs, failFlg
                     
-
-                # Error Handling
-                if cur_row == self.img_height*2 - 2:
-                    # Validate Image - check pixel with 0 value
-                    # Depth
-                    zero_cnt_depth = np.where(imgDepth == 0)[0]
-                    if zero_cnt_depth.size > 900:
-                        # print('Fail - Bad image')
-                        failFlg = 2
-                    # Intensity
-                    zero_cnt_inten = np.where(imgInten == 0)[0]
-                    if zero_cnt_inten.size > 900:
-                        # print('Fail - Bad image')
-                        failFlg = 2
-
-                    """
-                    
-        return dcs, failFlg
+        
 
 
 if __name__ == '__main__':
     ilidar = iLidarDriver()
     dcs, failFlg = ilidar.iLidar_getImg()
-    time.sleep(1)
+    # time.sleep(0.1)
     dcs, failFlg = ilidar.iLidar_getImg()
-    time.sleep(1)
+    # time.sleep(0.1)
     dcs, failFlg = ilidar.iLidar_getImg()
